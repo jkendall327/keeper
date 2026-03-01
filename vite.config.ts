348a1f76e72105e3c2c36cba623792e7241648a2
jackkendall <jkendall3096@gmail.com>
2026-03-01 14:32:20 +0000

    Allow access from phone

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: "0.0.0.0",
+    allowedHosts: [".ts.net"],
     proxy: {
       "/api": "http://localhost:3001",
     },
